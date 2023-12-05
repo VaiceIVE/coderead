60a2e79fbb66d6124aad096423f4b6a7ad5c15f0
Isachenko Ivan <vanisachenko@gmail.com>
2023-12-05 14:01:21 +0600

    fix: database type fix

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -7,7 +7,7 @@ export const databaseProviders = [
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
-        type: "mysql",
+        type: "mssql",
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
